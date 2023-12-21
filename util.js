74aa3ce3c3802b84b0ddb61edc20f5476f30820d
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2023-12-21 18:02:11 +0200

    refactored model

---
+++ util.js
@@ -0,0 +1,14 @@
+function checkEnteredNumber(str) {
+  return parseInt(str) || 0;
+}
+
+function checkToken(tokenName) {
+  if (tokenName.length < 3) {
+    renderModalSmallTokenName();
+  } else if (model.isTokenExist(tokenName)) {
+    renderModalAlreadyExsist();
+  } else {
+    model.addToken(tokenName);
+    renderToken(tokenName);
+  }
+}
